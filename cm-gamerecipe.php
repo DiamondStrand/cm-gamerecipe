26ababbe69a110d612412f93b324bfaf0826c431
CookifyMedia <sportbilsgiganten@gmail.com>
2024-10-22 15:43:20 +0200

    change sizie on image uploads

--- cm-gamerecipe.php
+++ cm-gamerecipe.php
@@ -3,7 +3,7 @@
 Plugin Name: CM Gamerecipe
 Plugin URI: https://github.com/DiamondStrand/cm-gamerecipe
 Description: Ett flexibelt och kraftfullt plugin för att skapa och hantera spelrecept. Med CM Gamerecipe kan du enkelt lägga till spel med detaljerade regler, antal deltagare, material, speltid, och andra spelrelaterade data.
-Version: 1.0.4
+Version: 1.0.5
 Author: Diamond Strand - CookifyMedia
 Text Domain: cm-gamerecipe
 Domain Path: /languages
